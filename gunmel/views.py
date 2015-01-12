677dcf955421b8e3ba542b2d7dd518ece44b4466
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2015-01-11 18:56:06 -0800

    basic plotting works

--- gunmel/views.py
+++ gunmel/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.dates import DateFormatter
 from matplotlib.figure import Figure
+from matplotlib.ticker import FormatStrFormatter, FixedLocator
 import mmh3
 
 from gunmel.models import Product, PriceHistory
@@ -42,6 +43,8 @@ class PriceHistoryView(DetailView):
 
 
 class ChartView(View):
+	_last_n = 5
+
 	def __init__(self, *arg, **kwargs):
 		super(ChartView, self).__init__(*arg, **kwargs)
 
@@ -50,21 +53,46 @@ class ChartView(View):
 		product = get_object_or_404(Product, pk=pk)
 
 		history = PriceHistory.objects.price_history(product)
-		dates, prices = zip(* map(lambda entry: (entry.timestamp, float(entry.price)), history))
 
 		response = HttpResponse(content_type='image/png')
-		self.populate(product, dates, prices, response)
+		self.populate(product, history, response)
 		return response
 
-	def populate(self, product, dates, prices, response):
+	def populate(self, product, history, response):
+
+		date_price_list = map(lambda entry: (entry.timestamp, float(entry.price)), history)
+		min_price_date, min_price = min(date_price_list, key=lambda (timestamp, price): price)
+		max_price_date, max_price = max(date_price_list, key=lambda (timestamp, price): price) 
+		dates, prices = zip(* date_price_list)
+
 		fig = Figure(frameon=False)
 		ax = fig.add_subplot(111)
-		ax.step(dates, prices)
+		ax.step(dates, prices)		
 		ax.xaxis_date()
 		ax.xaxis.set_major_formatter(DateFormatter('%b-%d-%Y'))
+		ax.yaxis.set_major_formatter(FormatStrFormatter('$%d'))
 		ax.grid()
 		ax.set_title(product.headline)
 		fig.autofmt_xdate()
+
+		min_price = min(prices)
+		max_price = max(prices)
+		ax2 = ax.twinx()
+		ax2.axhline(y=min_price, color='g', linestyle='--')
+		ax2.axhline(y=max_price, color='r', linestyle='--')
+		ax2.yaxis.set_major_formatter(FormatStrFormatter('$%0.2f'))
+		ax2.yaxis.set_major_locator(FixedLocator([min_price, max_price]))
+		#min_price_tick_label_text = '$%0.2f\n%s' % (min_price, min_price_date.strftime('%b')) 
+		#max_price_tick_label_text = '$%0.2f\n%s' % (max_price, max_price_date.strftime('%b'))
+
+		#ax2.set_yticklabels(min_price_tick_label_text, max_price_tick_label_text)
+		min_price_tick_label, max_price_tick_label = ax2.get_yticklabels()
+		min_price_tick_label.set_color('green')
+		max_price_tick_label.set_color('red')
+		# ax2.annotate('arc', xy=(dates[5], max_price), xycoords='data', xytext=(-30, -30), textcoords='offset points', arrowprops=dict(arrowstyle='->',
+		# 	connectionstyle='arc,angleA=0,armA=30,rad=10'))
+
+		fig.tight_layout()
 		canvas = FigureCanvasAgg(fig)
 		canvas.print_png(response)
 		
